47bad4fd1e17b98b9f5267eaf192bcb175e51f34
eleceb <cheungcaleb1030@gmail.com>
2024-08-29 21:35:50 +0800

    Fixed minor bugs

--- Color Dash Galaxy/Assets/Scripts/UI/MainMenuScreen.cs
+++ Color Dash Galaxy/Assets/Scripts/UI/MainMenuScreen.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class MainMenuScreen : MonoBehaviour
